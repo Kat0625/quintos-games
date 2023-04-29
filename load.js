3d3e96d5fbcc44bb5d9eed438e9ef14d4476f45b
Kat0625 <katrina.eleanor.lee@gmail.com>
2023-04-29 12:11:37 -0400

    1.1.12

--- load.js
+++ load.js
@@ -1,3 +1,3 @@
-QuintOS.user = 'your-username'; // put your github username!
-QuintOS.dir = '.'; // the folder with the game folder in it
-QuintOS.game = 'HelloWorld'; // the title of the game to load
+QuintOS.user = 'Kat0625'; // put your github username!
+QuintOS.dir = 'GAMES'; // the folder with the game folder in it
+QuintOS.game = 'QuickClicks'; // the title of the game to load
